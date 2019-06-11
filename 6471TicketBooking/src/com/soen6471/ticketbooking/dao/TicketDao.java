1a7169399413bf7f9f766b0b06d4cbf37f994946
Kiranmayie <47037511+Kiranmayie@users.noreply.github.com>
2019-06-11 19:50:42 -0400

    DBConnection

--- 6471TicketBooking/src/com/soen6471/ticketbooking/dao/TicketDao.java
+++ 6471TicketBooking/src/com/soen6471/ticketbooking/dao/TicketDao.java
@@ -4,13 +4,15 @@
 package com.soen6471.ticketbooking.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.soen6471.ticketbooking.components.BookingInfoComponent;
-import com.soen6471.ticketbooking.connection.DBConnection;
+import com.soen6471.ticketbooking.components.UserComponent;
+
 
 /**
  * @author fly
@@ -21,29 +23,43 @@ public class TicketDao {
     Statement stmt = null;
     PreparedStatement preparedStmt=null;
     ResultSet rs = null;
-    DBConnection dbConnection=null;
+
     String sql=null;
     
-    public int updateBookingTable(BookingInfoComponent bookInfo)
+    public boolean checkLogin(UserComponent user)
     {
-    	int row=0;
-    	try {
-    	sql="INSERT INTO booking_info (?,?,?,?,?,?,?,?)";
-    	dbConnection =new DBConnection();
-    	conn = dbConnection.getDataSource().getConnection();
+    	boolean row=false;
+    	 try {
+    		 Class.forName("com.mysql.jdbc.Driver");
+			 Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/movie_table",
+    	      "root","root");
+   	 
+   	 if(con!=null)
+   	 {
+   		 System.out.println("Connected");
+		preparedStmt = con.prepareStatement("SELECT * FROM movie_table.User_Info where email_id=? and password=?");
+		preparedStmt.setString(1, user.getEmail());
+		preparedStmt.setString(2, user.getPassword());
 		
-    	preparedStmt = conn.prepareStatement(sql);
-    	//preparedStmt.setString(2, );
-        
-    	
-    	
-    	
-    	
-    	} 
-    	catch (SQLException e) {
-			
-			e.printStackTrace();
+		ResultSet rs= preparedStmt.executeQuery();
+		
+		while(rs.next())
+		{
+			row=true;
 		}
+		
+   	 }
+	} 
+    	
+    	 catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	 catch (SQLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	   
     	return row;
     }
 
