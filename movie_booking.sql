e94a22fb179a3461ce7ee21dba780514e2d9f902
Sahana15Shankar <44722095+Sahana15Shankar@users.noreply.github.com>
2019-06-11 21:42:13 -0400

    Update movie_booking.sql
    tables are created according to mysql syntax

--- movie_booking.sql
+++ movie_booking.sql
@@ -1,82 +1,90 @@
-Create table User_Info (
-  user_Id integer IDENTITY(1,1) PRIMARY KEY,
-  Fname varchar(20) not null,
-  Lname  varchar(20),
-  Email_id  varchar(50) not null,
-  password varchar(20) not null,
-  contact int(10),
-   CONSTRAINT Fname_email UNIQUE (Fname,Email_id)
-  
-  )
-  
- Create table Admin_Info (
-   Admin_Id integer IDENTITY(1,1) PRIMARY KEY,
-  Fname varchar(20) not null,
-  Lname  varchar(20),
-  Email_id  varchar(50) not null,
-  password varchar(20) not null,
-  contact int(10),
-   CONSTRAINT Fname_email UNIQUE (Fname,Email_id)
-  )
-   
-  Create table Movie_Info (
-     Movie_Id integer IDENTITY(1,1) PRIMARY KEY,
-    Name_of_movie varchar(20) not null,
-    Description varchar(50),
-    Movie_length int,
-      CONSTRAINT mID_nameMov UNIQUE (Movie_Id,Name_of_movie)
-  )
-  
+create database movie_table;
  
-  
-  Create table Screen(
-     Screen_Id integer IDENTITY(1,1) PRIMARY KEY,
-    Movie_Id int,
-	FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id)
+Create table movie_table.User_Info (
+  user_Id integer auto_increment ,
+  Fname varchar(20) not null,
+  Lname  varchar(20),
+  Email_id  varchar(50) UNIQUE not null,
+  mpassword varchar(20) not null,
+  contact integer(10),
+   PRIMARY KEY (user_Id)
   )
-  
-  Create table Showtime (
+  ;
+
+ Create table movie_table.Admin_Info (
+   Admin_Id integer auto_increment ,
+  Fname varchar(20) not null,
+  Lname  varchar(20),
+  Email_id  varchar(50) UNIQUE not null,
+  mpassword varchar(20) not null,
+   PRIMARY KEY (Admin_Id)
+  );
+   
+
+  Create table movie_table.Movie_Info (
+     Movie_Id integer auto_increment ,
+    Name_of_movie varchar(20) not null,
+    mDescription varchar(50),
+    Movie_length int,
+      PRIMARY KEY (Movie_Id)
+  );
+
+
+
+  Create table movie_table.Screen(
+     Screen_Id integer auto_increment ,
+    Movie_Id int,
+FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
+    PRIMARY KEY (Screen_Id)
+  );
+
+  Create table movie_table.Showtime (
     Screen_Id int,
     Movie_Id int,
-      Showtime_Id integer IDENTITY(1,1) PRIMARY KEY,
-   
-	FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
-   
-	FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id)
+      Showtime_Id integer auto_increment ,
+
+FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
+
+FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id),
+    PRIMARY KEY (Showtime_Id)
   )
-  
-   Create table Seat(
+  ;
+   Create table movie_table.Seat(
      Movie_Id int,
-	 Screen_Id int,
-     Seat_Id integer IDENTITY(1,1) PRIMARY KEY,
-	FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
-	FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id)
-  )
-  
-   Create table Booking_Info (
+Screen_Id int,
+     Seat_Id integer auto_increment ,
+FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
+FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id),
+    PRIMARY KEY (Seat_Id)
+  );
+
+   Create table movie_table.Booking_Info (
     Screen_Id int,
-	 Movie_Id int,
-	 Seat_Id int,
-	 Showtime_Id int,
-	  user_Id int,
-     Booking_Id Bookinginteger IDENTITY(1,1) PRIMARY KEY,
-	FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
-	FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id)  
-	  FOREIGN KEY(Seat_Id) REFERENCES Seat(Seat_Id),  
-	  FOREIGN KEY(Showtime_Id) REFERENCES Showtime(Showtime_Id),
-	  FOREIGN KEY(user_Id) REFERENCES User_Info(user_Id)
-     
-  )
-  
-  create table Credit_info(
-credit_Id integer IDENTITY(1,1) PRIMARY KEY,
+Movie_Id int,
+Seat_Id int,
+Showtime_Id int,
+  user_Id int,
+     Booking_Id integer auto_increment ,
+FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
+FOREIGN KEY(Screen_Id) REFERENCES Screen(Screen_Id)  ,
+  FOREIGN KEY(Seat_Id) REFERENCES Seat(Seat_Id),
+  FOREIGN KEY(Showtime_Id) REFERENCES Showtime(Showtime_Id),
+  FOREIGN KEY(user_Id) REFERENCES User_Info(user_Id),
+      PRIMARY KEY (Booking_Id )
+
+  );
+
+  create table movie_table.Credit_info(
+credit_Id integer auto_increment ,
  credit_number integer(20) ,
-  type varchar(10),
+  mtype varchar(10),
   expDate varchar(10),
-  Booking_Id varchar(10),
+  Booking_Id integer,
    Movie_Id int,
      user_Id int,
   FOREIGN KEY(Booking_Id) REFERENCES Booking_Info(Booking_Id),
-	FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
-	  FOREIGN KEY(user_Id) REFERENCES User_Info(user_Id)
+FOREIGN KEY(Movie_Id) REFERENCES Movie_Info(Movie_Id),
+  FOREIGN KEY(user_Id) REFERENCES User_Info(user_Id),
+      PRIMARY KEY (credit_Id)
 )
+;
